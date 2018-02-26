3ceb256a297f65b812ff596226829eab41d15d59
Lucas Gonçalves de Paula <lucaslucasfabio@hotmail.com>
2018-02-26 03:18:54 -0300

    Adicionando regra
    adicionando regras para altura que ira mostrar o mapa e entao calculando as coordenadas para retorno

--- api/services/postoCoordService.js
+++ api/services/postoCoordService.js
@@ -3,8 +3,8 @@
 var mongoose = require('mongoose'),
     Posto = mongoose.model('Postos'),
     validator = require('validator'),
-    undefinedVerify = require('../util/undefinedVerify'),
-    code, msg;
+    code = 400,
+    msg = "Erro na requisição, repita ou tente mais tarde";
 
 exports.calcularCoord = function (req) {
     if (validator.isNumeric(req.altura)) {
@@ -12,14 +12,72 @@ exports.calcularCoord = function (req) {
                 min: 13,
                 max: 16
             })) {
+            function verificaZeroNove(str, altura) {
+                var quanto
+                if (altura === 15)
+                    quanto = 1;
+                else if (altura === 16)
+                    quanto = 2;
+                else if (altura === 17)
+                    quanto = 4;
+                else {
+                    quanto = 1;
+                }
+
+                var stringArray = Array.from(str[1]);
+                var valor = parseInt(stringArray[0]);
+                if (valor - quanto >= 0 && valor + quanto <= 9) {
+                    stringArray[0] = valor - quanto;
+                    var abaixo = str[0] + "." + stringArray.join(['']);
+                    stringArray[0] = valor + quanto;
+                    var acima = str[0] + "." + stringArray.join(['']);
+                    return {
+                        acima: acima,
+                        abaixo: abaixo
+                    }
+                } else if (valor - quanto < 0) {
+                    stringArray[0] = valor + quanto;
+                    var acima = str[0] + "." + stringArray.join(['']);
+                    stringArray[0] = 10 - (quanto - valor);
+                    if (str[0] < 0) {
+                        str[0] = parseInt(str[0]) + 1;
+                    } else {
+                        str[0] = parseInt(str[0]) - 1;
+                    }
+                    var abaixo = str[0] + "." + stringArray.join(['']);
+                    return {
+                        acima: acima,
+                        abaixo: abaixo
+                    }
+                }
+                stringArray[0] = valor - quanto;
+                var abaixo = str[0] + "." + stringArray.join(['']);
+                stringArray[0] = quanto;
+                if (str[0] < 0) {
+                    str[0] = parseInt(str[0]) - 1;
+                } else {
+                    str[0] = parseInt(str[0]) + 1;
+                }
+                var acima = str[0] + "." + stringArray.join(['']);
+                return {
+                    acima: acima,
+                    abaixo: abaixo
+                }
+            }
+            var calculaLong = req.longitude.split(".");
             var calculaLat = req.latitude.split(".");
-            var latCalculada = Array.from(calculaLat[1]);
-            latCalculada[0] = parseInt(latCalculada[0]) + 1;
-            calculaLat = calculaLat[0] + "." + latCalculada.join(['']);
-            console.log(req.latitude +"     "+calculaLat)
+            var latitude = verificaZeroNove(calculaLat);
+            var longitude = verificaZeroNove(calculaLong);
+            console.log(latitude)
+            console.log(longitude)
             Posto.find({
                 "latitude": {
-                    $lt: calculaLat
+                    $gt: latitude.abaixo,
+                    $lt: latitude.acima
+                },
+                "longitude": {
+                    $gt: longitude.abaixo,
+                    $lt: longitude.acima
                 }
             }, function (err, task) {
                 if (err) {
