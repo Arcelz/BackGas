c2a3f0e041265e3f3a4e5916410580a5b3e125a1
Lucas Gonçalves de Paula <lucaslucasfabio@hotmail.com>
2018-03-05 06:51:49 -0300

    cors
    adicionandos cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 8000,
+  cors = require('cors'),
   mongoose = require('mongoose'),
   Posto = require('./api/models/postoModel'), //criando o modelo
   bodyParser = require('body-parser');
@@ -14,10 +15,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use(cors({origin: 'http://localhost:8080'}));
+
 var routes = require('./api/routes/postoRoutes'); //adicionando a rota
 var routes2 = require('./api/routes/postoCoordRoutes');
 routes(app); //register the route
 routes2(app);
+
 
 app.use(function (req, res) {
   res.status(404).send({
